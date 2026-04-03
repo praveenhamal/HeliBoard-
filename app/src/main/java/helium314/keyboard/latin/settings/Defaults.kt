3a24ccfba3a2a5b003d882c04004912b6b265fc8
Your Name <you@example.com>
2026-04-03 19:25:34 +0545

    minor bug fix

--- app/src/main/java/helium314/keyboard/latin/settings/Defaults.kt
+++ app/src/main/java/helium314/keyboard/latin/settings/Defaults.kt
@@ -164,7 +164,7 @@ object Defaults {
     const val PREF_ABC_AFTER_SYMBOL_SPACE = true
     const val PREF_ABC_AFTER_NUMPAD_SPACE = false
     const val PREF_REMOVE_REDUNDANT_POPUPS = false
-    const val PREF_SPACE_BAR_TEXT = "JSPN"
+    const val PREF_SPACE_BAR_TEXT = ""
     const val PREF_TIMESTAMP_FORMAT = "yyyy-MM-dd HH:mm:ss"
     const val PREF_EMOJI_RECENT_KEYS = ""
     const val PREF_LAST_SHOWN_EMOJI_CATEGORY_PAGE_ID = 0
